1e828ca2b3e06b75a185ca824f24a11558eb8704
ConorC90 <47102093+ConorC90@users.noreply.github.com>
2024-12-21 12:32:34 +0100

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import * as Craft from "@/components/craft/layout";
 import HomePage from "./home/page";
-import Logo from "@/public/placeholder-2.jpg";
-import { Parallax } from "react-scroll-parallax";
-import ParallaxComponent from "../components/craft/parallax";
 import Test from "./test";
 
 export default function Home() {
